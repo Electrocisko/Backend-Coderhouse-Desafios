75053aeddaa3030ef934d609a07e8a9ce4501994
Francisco Zuidwijk <electrocisko@gmail.com>
2022-09-06 22:28:43 -0300

    se agrego un archivo authors.json

--- desafio-11-normalize/src/database/servicesMongdb/servicesMongodb.js
+++ desafio-11-normalize/src/database/servicesMongdb/servicesMongodb.js
@@ -1,60 +1,59 @@
-import mongoose from 'mongoose';
-import authorsService from '../modelsMongoo/authors.js';
+import mongoose from "mongoose";
+import authorsService from "../modelsMongoo/authors.js";
 import chatService from "../modelsMongoo/chats.js";
-import {normalize, schema} from 'normalizr';
-const URL = 'mongodb://127.0.0.1:27017/ecommerce1';
-const URL_MONGO = 'mongodb+srv://zuchi:xkT3ZDTSXyDv4hB@cluster0.rvl2uyz.mongodb.net/ecomercceA?retryWrites=true&w=majority'
+import { normalize, schema } from "normalizr";
+const URL = "mongodb://127.0.0.1:27017/ecommerce1";
+const URL_MONGO =
+  "mongodb+srv://zuchi:xkT3ZDTSXyDv4hB@cluster0.rvl2uyz.mongodb.net/ecomercceA?retryWrites=true&w=majority";
 
-let mensajes=[];
+let mensajes = [];
 
-export const conectMongo = async () =>{
-  mongoose.connect(URL_MONGO, err => {
-    if(err){
-      console.log(err)
+export const conectMongo = async () => {
+  mongoose.connect(URL_MONGO, (err) => {
+    if (err) {
+      console.log(err);
     } else {
-      console.log('Conected to database')
+      console.log("Conected to database");
     }
-  })
-}
-
-const process = async () => {
-let authors = await authorsService.find({},{'email':1});
-  //mongoose.disconnect();
-} 
-process();
-
-export const saveChatDB = async (data) => {
-  let mensajes = await chatService.create(data)
-  return (mensajes)
-}
-
-export const muestroChats = async () => {
-  let chats = await chatService.find().populate('author');
-  return chats
+  });
 };
 
-export const getIdAuthor = async(data) => {
-  let result = await authorsService.find({'email':data}) 
-  if (result.length === 0) return '63127ed7d964b9e19e41df0d'
+export const saveChatDB = async (data) => {
+  let mensajes = await chatService.create(data);
+  return mensajes;
+};
+
+export const muestroChats = async () => {
+  let chats = await chatService.find().populate("author");
+  return chats;
+};
+
+export const getIdAuthor = async (data) => {
+  let result = await authorsService.find({ email: data });
+  if (result.length === 0) return "63127ed7d964b9e19e41df0d";
   let objectid = result[0]._id;
-  return objectid
-}
+  return objectid;
+};
 
 export const chatsNormalized = async () => {
   let chats = await muestroChats();
   let mensajes = {
-    id: '1000',
-    mensajes: chats
-  }
-///NORMALIZE
-const authorSchema = new schema.Entity('authors');
-const mensajeSchema = new schema.Entity('mensajes', {
-  author: authorSchema,
-});
-const blogSchema = new schema.Entity('post',{
-  mensajes: [mensajeSchema]
-});
-const normalizedData = normalize(mensajes,blogSchema);
+    id: "mensajes",
+    mensajes: chats,
+  };
 
-return normalizedData
+  ///NORMALIZE
+  const authorSchema = new schema.Entity("authors");
+
+  const mensajeSchema = new schema.Entity("mensajes",
+   { author: authorSchema }
+   );
+
+  const blogSchema = new schema.Entity("post",{ 
+    mensajes: [mensajeSchema]
+   });
+
+  const normalizedData = normalize(mensajes, blogSchema);
+
+  return normalizedData;
 };
