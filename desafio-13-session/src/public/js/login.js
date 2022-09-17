675915b85bdf3fefc61ee8b5bc11a2284c94fd94
Francisco Zuidwijk <electrocisko@gmail.com>
2022-09-17 18:16:01 -0300

    Se agrega vistas de errores en handlebars

--- desafio-13-session/src/public/js/login.js
+++ desafio-13-session/src/public/js/login.js
@@ -3,7 +3,7 @@ const form = document.getElementById("loginForm");
 
  const succes = (data) => {
   console.log('login.js',data)
-    if (data.status === 'error') {alert('Usuario no valido')}
+    if (data.status === 'error') {window.location.assign("http://localhost:8080/errorlogin");}
     else {
     window.location.assign("http://localhost:8080/nuevoProducto");
     }
--- desafio-13-session/src/routes/views.router.js
+++ desafio-13-session/src/routes/views.router.js
@@ -12,7 +12,7 @@ const router = Router();
 
 router.get('/',(req,res) => {
   res.render('inicio')
-})
+});
 
 router.get("/productos", async (req, res) => {
     let products = await userService.getAll();
@@ -26,10 +26,18 @@ router.get('/nuevoProducto', (req,res )=> {
 
 router.get('/login', (req,res) => {
   res.render('login');
-})
+});
 
 router.get('/register',(req,res) => {
   res.render('register');
-})
+});
+
+router.get('/errorlogin',(req,res) => {
+  res.render('errorLogin')
+});
+
+router.get('/errorregister',(req,res) => {
+  res.render('errorRegister')
+});
 
 export default router;
