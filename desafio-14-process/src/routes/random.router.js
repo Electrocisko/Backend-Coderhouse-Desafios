30fa806b8c677f2a72e0d6e93fe4a16d6d0f4b69
Francisco Zuidwijk <electrocisko@gmail.com>
2022-09-30 14:24:58 -0300

    Desafio 14, al get /api/random se le agrego para mostrar el resultado en un array

--- desafio-14-process/src/routes/random.router.js
+++ desafio-14-process/src/routes/random.router.js
@@ -3,6 +3,7 @@ import { fork } from "child_process";
 import __dirname from "../utils.js";
 
 const router = new Router();
+let aux;
 
 router.get("/random", (req, res) => {
   let q = req.query.cant;
@@ -10,7 +11,8 @@ router.get("/random", (req, res) => {
   const child = fork(__dirname+"/others/random.js");
   child.send(q);
   child.on("message", (val) => {
-    res.send("Resultado "+JSON.stringify(val));
+    aux = Object.entries(val); // Para mostrarlos en un array
+    res.send(aux)
   });
 });
 
